40862c0a18c447f29613704a58c980b66d221e27
mknorps <maria.knorps@tweag.io>
2022-11-07 13:54:47 +0100

    WIP haskell overlay updates of monad-bayes

--- monad-bayes-series/flake.nix
+++ monad-bayes-series/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Probabilistic Programming in Haskell blog resourses";
   inputs = {
-    nixpkgs.url = "nixpkgs/22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
     jupyterWith.url = "github:tweag/jupyterWith";
     flake-utils.url = "github:numtide/flake-utils";
   };
@@ -9,10 +9,9 @@
   outputs = { self, nixpkgs, jupyterWith, flake-utils }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
       let
-        inherit (nixpkgs) lib;
         pkgs = import nixpkgs {
           system = system;
-          overlays = (nixpkgs.lib.attrValues jupyterWith.overlays) ++ [ (import ./haskell-overlay.nix) ];
+          overlays = (builtins.attrValues jupyterWith.overlays) ++ [ (import ./haskell-overlay.nix) ];
         };
         iHaskell = pkgs.kernels.iHaskellWith {
           name = "monad-bayes-series-env";
