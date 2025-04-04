b017a1979da5b2a86bbcf13c1d0153a5bab4319f
Yann Hamdaoui <yann.hamdaoui@tweag.io>
2025-04-04 17:18:11 +0200

    Add more comment to the code

--- recursion-schemes-in-rust/src/lib.rs
+++ recursion-schemes-in-rust/src/lib.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+/// The original, "naive" representation of spanned JSON values without using recursion schemes.
 mod span {
     pub type Span = std::ops::Range<usize>;
 
@@ -19,10 +20,7 @@ mod span {
     }
 }
 
-pub struct JsonValue {
-    data: JsonValueF<Box<JsonValue>>,
-}
-
+/// The functor representation.
 pub enum JsonValueF<T> {
     String(String),
     Number(f64),
@@ -44,19 +42,28 @@ pub enum JsonValueRefF<'a, T> {
 
 pub type JsonValueRef<'a, T> = JsonValueRefF<'a, &'a T>;
 
+/// A simple JSON value from the functor representation.
+pub struct JsonValue {
+    data: JsonValueF<Box<JsonValue>>,
+}
+
+/// Same as [span::SpannedValue], but using the functor representation.
 pub struct SpannedJsonValue {
     data: JsonValueF<Box<SpannedJsonValue>>,
     span: std::ops::Range<u32>,
 }
 
+/// A non-owned, shared version of JSON values.
 pub struct SharedJsonValue {
     data: JsonValueF<std::rc::Rc<JsonValue>>,
 }
 
+/// A borrowed version of JSON values.
 pub struct ArenaJsonValue<'a> {
     data: JsonValueF<&'a JsonValue>,
 }
 
+/// Count the number of string leaves, without using recursion schemes.
 pub fn count_strings_naive(value: &JsonValue) -> u32 {
     match &value.data {
         JsonValueF::String(_) => 1,
@@ -67,6 +74,7 @@ pub fn count_strings_naive(value: &JsonValue) -> u32 {
     }
 }
 
+/// Count the number of string leaves, using the `map` combinator, as in the blog post.
 pub fn count_strings_map(value: JsonValue) -> u32 {
     match value.data.map(|unr| count_strings_map(*unr)) {
         JsonValueF::String(_) => 1,
@@ -77,6 +85,8 @@ pub fn count_strings_map(value: JsonValue) -> u32 {
     }
 }
 
+/// Count the number of string leaves but take the argument by reference, using `fold` instead.
+/// This example isn't in the post.
 pub fn count_strings_ref(value: &JsonValue) -> u32 {
     value.fold(&mut |unr| match unr {
         JsonValueRefF::String(_) => 1,
@@ -84,10 +94,11 @@ pub fn count_strings_ref(value: &JsonValue) -> u32 {
         JsonValueRefF::Pair(first, second) => first + second,
         JsonValueRefF::Array(array) => array.iter().sum(),
         JsonValueRefF::Object(object) => object.values().sum(),
-    }) 
+    })
 }
 
 impl<T> JsonValueF<T> {
+    /// The core `map` combinator.
     pub fn map<U>(self, mut f: impl FnMut(T) -> U) -> JsonValueF<U> {
         match self {
             JsonValueF::String(s) => JsonValueF::String(s),
@@ -113,15 +124,18 @@ impl<T> JsonValueF<T> {
             JsonValueF::Array(array) => {
                 JsonValueRefF::Array(array.into_iter().map(|elt| f(elt)).collect())
             }
-            JsonValueF::Object(object) => {
-                JsonValueRefF::Object(object.into_iter().map(|(k, v)| (k.as_str(), f(v))).collect())
-            }
+            JsonValueF::Object(object) => JsonValueRefF::Object(
+                object
+                    .into_iter()
+                    .map(|(k, v)| (k.as_str(), f(v)))
+                    .collect(),
+            ),
         }
     }
-
 }
 
 impl JsonValue {
+    /// Rewrite all subtrees of a JSON value by applying a function to it.
     pub fn map_bottom_up(self: JsonValue, f: &mut impl FnMut(JsonValue) -> JsonValue) -> JsonValue {
         let data = self.data.map(|v| Box::new(v.map_bottom_up(f)));
         f(JsonValue { data })
@@ -135,10 +149,7 @@ impl JsonValue {
     /// Another common presentation, instead of taking one function from `JsonValueRefF<A>` as
     /// an argument, is to take one function per variant: `fn string(&str) -> A`, `fn number(&f64)
     /// -> A`, `fn object(&HashMap<&str, A>) -> A`, etc. The two are presentations equivalent.
-    pub fn fold<A>(
-        &self,
-        f: &mut impl FnMut(JsonValueRefF<A>) -> A,
-    ) -> A {
+    pub fn fold<A>(&self, f: &mut impl FnMut(JsonValueRefF<A>) -> A) -> A {
         let unrolled = self.data.map_ref(|unr| unr.fold(f));
         f(unrolled)
     }
